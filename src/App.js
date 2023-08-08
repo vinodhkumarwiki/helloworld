47702fe97d1a346c400250ce94d421899533737a
Vinodh Kumar <50162535+vinodhkumar007@users.noreply.github.com>
2023-08-09 01:02:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from our team
+          Learn React from our professional team
         </a>
       </header>
     </div>
